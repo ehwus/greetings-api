74bf9d7cdf6c23504e96ed505d2403cdd7f660a4
Alex Withington-Smith <alex@awithsmith.com>
2023-02-06 09:42:53 +0000

    cache the JSON just in case this sees actual usage lol

--- main.js
+++ main.js
@@ -4,6 +4,14 @@ const fs = require("fs");
 
 const port = process.env.PORT || 3000;
 
+// Read and cache the JSON.
+let greetings;
+fs.readFile("hello.json", (err, data) => {
+    if (err) throw err;
+    greetings = JSON.parse(data);
+});
+
+// Enable CORS
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header(
@@ -13,13 +21,10 @@ app.use((req, res, next) => {
     next();
 });
 
+// Serve route
 app.get("/api/greetings", (req, res) => {
-    fs.readFile("hello.json", (err, data) => {
-        if (err) throw err;
-        const greetings = JSON.parse(data);
-        const randomIndex = Math.floor(Math.random() * greetings.length);
-        res.json(greetings[randomIndex]);
-    });
+    const randomIndex = Math.floor(Math.random() * greetings.length);
+    res.json(greetings[randomIndex]);
 });
 
 app.listen(port, () => {
